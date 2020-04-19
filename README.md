10486c4ae2cc323ba422da8fe358761cb2fe9887
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-04-19 17:42:10 +0100

    finding comment with dict key

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # gradex-extract
 get the data out of a gradex-enabled pdf. Note that usually only the right-hand most side bar is active
+
+
+Note the issues highlighted [here with ambiguities in the PDF ecosystem](https://gendignoux.com/blog/2016/10/19/pdf-parsing-pitfalls.html)
--- gradex-extract.go
+++ gradex-extract.go
@@ -11,10 +11,16 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	unicommon "github.com/unidoc/unipdf/v3/common"
-	"github.com/unidoc/unipdf/v3/model"
+	pdfcore "github.com/unidoc/unipdf/v3/core"
+	pdf "github.com/unidoc/unipdf/v3/model"
 )
+
+type cmdOptions struct {
+	pdfPassword string
+}
 
 func main() {
 	// When debugging, enable debug-level logging via console:
@@ -36,6 +42,16 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+
+	var opt cmdOptions
+
+	fmt.Printf("Input file: %s\n", pdfPath)
+	err = inspectPdf(pdfPath, opt)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
 }
 
 func printPdfFieldData(inputPath, targetFieldName string) error {
@@ -47,7 +63,7 @@ func printPdfFieldData(inputPath, targetFieldName string) error {
 
 	defer f.Close()
 
-	pdfReader, err := model.NewPdfReader(f)
+	pdfReader, err := pdf.NewPdfReader(f)
 	if err != nil {
 		return err
 	}
@@ -78,5 +94,126 @@ func printPdfFieldData(inputPath, targetFieldName string) error {
 	if !match {
 		return errors.New("field not found")
 	}
+	return nil
+}
+
+func inspectPdf(inputPath string, opt cmdOptions) error {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	pdfReader, err := pdf.NewPdfReader(f)
+	if err != nil {
+		return err
+	}
+
+	isEncrypted, err := pdfReader.IsEncrypted()
+	if err != nil {
+		return err
+	}
+
+	// Try decrypting with an empty one.
+	if isEncrypted {
+		auth, err := pdfReader.Decrypt([]byte(opt.pdfPassword))
+		if err != nil {
+			return err
+		}
+
+		if !auth {
+			return errors.New("Unable to decrypt password protected file - need to specify pass to Decrypt")
+		}
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("PDF Num Pages: %d\n", numPages)
+
+	objNums := pdfReader.GetObjectNums()
+
+	// Output.
+	fmt.Printf("%d PDF objects:\n", len(objNums))
+	for _, objNum := range objNums {
+		obj, err := pdfReader.GetIndirectObjectByNumber(objNum)
+		if err != nil {
+			return err
+		}
+		//fmt.Printf("%3d: %d 0 %T\n", i, objNum, obj)
+		/*if stream, is := obj.(*pdfcore.PdfObjectStream); is {
+			//decoded, err := pdfcore.DecodeStream(stream)
+			if err != nil {
+				return err
+			}
+			//fmt.Printf("Decoded:\n%s\n", decoded)
+		} else
+		*/
+		if indObj, is := obj.(*pdfcore.PdfIndirectObject); is {
+
+			//fmt.Println(indObj.PdfObject.String())
+
+			/*
+				switch v := foo.Get("Name").(type) {
+				default:
+				case string:
+					fmt.Println(foo.Get("Contents"))
+				}*/
+
+			if foo, is := indObj.PdfObject.(*pdfcore.PdfObjectDictionary); is {
+
+				v := fmt.Sprintf("%s", foo.Get("Name"))
+				if strings.Compare(v, "Comment") == 0 {
+					fmt.Println("=========================================================")
+					fmt.Println(foo.Get("Contents"))
+					fmt.Println(foo.Get("Rect"))
+					//for _, k := range foo.Keys() {
+					//	fmt.Printf("%s:%s\n", k, foo.Get(k))
+					//}
+				}
+				//	fmt.Println(v)
+
+				/*
+					switch v := foo.Get("Name").(type) {
+									default:
+									case string:
+										if strings.Equal(v, "Contents") {
+											fmt.Println(foo.Get("Contents"))
+										}
+									}*/
+
+				//fmt.Println(foo.Get("Contents"))
+				if false {
+					for _, k := range foo.Keys() {
+						fmt.Printf("%s:%s\n", k, foo.Get(k))
+					}
+				}
+			}
+
+			//if objDict, is := obj.(*pdfcore.PdfObjectDictionary); is {
+			//	fmt.Printf("FOUND ONE: %T \n", objDict)
+			//}
+			/*
+				for _, k := range indObj.PdfObject.PdfObjectDictionary.keys {
+					fmt.Printf("%s:%s\n", k, indObj.PdfObject[k])
+				}*/
+
+			//if objDict, is := obj.(*pdfcore.PdfObjectDictionary); is {
+
+			//fmt.Printf("FOUND ONE: %T \n", indObj.PdfObject) //.PdfObjectDictionary)
+			//fmt.Printf("%T\n", indObj.PdfObject)
+
+			//fmt.Printf("%s\n", indObj.PdfObject.String())
+
+			//contents := indObj.PdfObject.String()
+
+			//}
+
+		}
+
+	}
+
 	return nil
 }
